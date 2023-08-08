878f43c2b3cd79ca4d004a8412012e91b2651805
Oğuzhan  Çevik <oguzhan728@gmail.com>
2023-08-08 10:31:20 +0300

    inital commit to try

---
+++ hooks/usePermission.js
@@ -0,0 +1,26 @@
+export const usePermissions = async () => {
+  let permissions = {
+    audio: false,
+    video: false,
+  };
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    });
+    console.log(stream.getTracks());
+    for (const track of stream.getTracks()) {
+      track.stop();
+    }
+    permissions = { video: true, audio: true };
+  } catch (err) {
+    permissions = { video: false, audio: false };
+    console.error(err);
+  }
+  // If we still don't have permissions after requesting them display the error message
+  if (!permissions.video) {
+    alert("Failed to get video permissions.");
+  } else if (!permissions.audio) {
+    alert("Failed to get audio permissions.");
+  }
+};
